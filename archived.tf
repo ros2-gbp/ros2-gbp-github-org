460337b0daecea7c5a136377e9cd6dfc972144bd
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-06-08 11:37:42 -0700

    Add a file for release repositories which should be archived.

---
+++ archived.tf
@@ -0,0 +1,21 @@
+locals {
+  archived_repositories = [
+    "ament_python-release",
+    "ament_tools-release",
+    "ceres_solver-release",
+    "console_bridge-release",
+    "gmock_vendor-release",
+    "gtest_vendor-release",
+    "osrf_testings_tools_cpp-release",
+    "poco_vendor-release",
+    "robot_model-release",
+    "system_tests-release",
+    "uncrustify-release",
+  ]
+}
+
+resource "github_repository" "archived_repositories" {
+  for_each = toset(local.archived_repositories)
+  name = each.value
+  archived = true
+}
