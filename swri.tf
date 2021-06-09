79b753a602728bf47ceb85a047248e87dd22a6be
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-06-09 12:51:47 -0700

    Expand marti team to SWRI.

---
+++ swri.tf
@@ -0,0 +1,46 @@
+# vim:ft=terraform
+
+locals {
+  swri_team = [
+    "evenator",
+    "pjreed",
+  ]
+  swri_repositories = [
+    "gps_umd-release",
+    "marti_common-release",
+    "marti_messages-release",
+  ]
+}
+
+resource "github_membership" "swri_team" {
+  for_each = toset(local.swri_team)
+  username = each.value
+  role = "member"
+}
+
+resource "github_team" "swri_team" {
+  name = "marti"
+  description = "ROS release managers for the marti project"
+  privacy = "closed"
+  create_default_maintainer = false
+}
+
+resource "github_team_membership" "swri_team" {
+  for_each = toset(local.swri_team)
+  team_id = github_team.swri_team.id
+  username = each.value
+  role = "member"
+}
+
+resource "github_repository" "swri_repositories" {
+  for_each = toset(local.swri_repositories)
+  name = each.value
+  visibility = "public"
+}
+
+resource "github_team_repository" "swri_team" {
+  for_each = toset(local.swri_repositories)
+  team_id = github_team.swri_team.id
+  repository = each.value
+  permission = "maintain"
+}
