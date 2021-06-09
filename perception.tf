92117c7e763911444a7f0518acaab3587fe23a2b
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-06-09 10:36:58 -0700

    Update perception team and repositories.

--- perception.tf
+++ perception.tf
@@ -2,10 +2,16 @@
 
 locals {
   perception_team = [
+    "SteveMacenski",
+    "paulbovbel",
     "jonbinney",
   ]
   perception_repositories = [
     "laser_geometry-release",
+    "pcl_msgs-release",
+    "perception_pcl-release",
+    "pointcloud_to_laserscan-release",
+    "radar_msgs-release",
   ]
 }
 
