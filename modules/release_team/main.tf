1c3c903810b7634b435b2ec8a5fcf50464b632ee
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-06-09 18:01:42 -0700

    Add module for release teams and make ros2_core special.

---
+++ modules/release_team/main.tf
@@ -0,0 +1,26 @@
+resource "github_team" "release_team" {
+  name = var.team_name
+  description = "ROS release managers for the ${var.team_name} project"
+  privacy = "closed"
+  create_default_maintainer = false
+}
+
+resource "github_team_membership" "release_team" {
+  for_each = toset(var.members)
+  team_id = github_team.release_team.id
+  username = each.value
+  role = "member"
+}
+
+resource "github_repository" "release_repositories" {
+  for_each = toset(var.repositories)
+  name = each.value
+  visibility = "public"
+}
+
+resource "github_team_repository" "release_team" {
+  for_each = toset(var.repositories)
+  team_id = github_team.release_team.id
+  repository = each.value
+  permission = "maintain"
+}
