8b322a3b7245db94461f933a0d26a9f82ce1a2f4
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-06-10 08:49:06 -0700

    Add vision_msgs releas team.

---
+++ vision_msgs.tf
@@ -0,0 +1,15 @@
+locals {
+  vision_msgs_team = [
+    "Kukanani",
+  ]
+  vision_msgs_repositories = [
+    "vision_msgs-release",
+  ]
+}
+
+module "vision_msgs_team" {
+  source = "./modules/release_team"
+  team_name = "vision_msgs"
+  members = local.vision_msgs_team
+  repositories = local.vision_msgs_team
+}
