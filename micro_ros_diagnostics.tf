b5d8e072a8d5abd6602ef9c3792d9520675a8bfa
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-10-27 16:53:03 -0700

    Add micro_ros_diagnostics and update system_modes.

---
+++ micro_ros_diagnostics.tf
@@ -0,0 +1,16 @@
+locals {
+  micro_ros_diagnostics_team = [
+    "norro",
+    "ralph-lange",
+  ]
+  micro_ros_diagnostics_repositories = [
+    "micro_ros_diagnostics-release",
+  ]
+}
+
+module "micro_ros_diagnostics_team" {
+  source = "./modules/release_team"
+  team_name = "micro_ros_diagnostics"
+  members = local.micro_ros_diagnostics_team
+  repositories = local.micro_ros_diagnostics_repositories
+}
