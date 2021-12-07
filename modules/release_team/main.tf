bde671eb076ee95163115d01d12c0c766d633466
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-12-06 16:45:03 -0800

    Ignore etag properties on resources.
    These will change whenever anything about the resource does and are not
    controllable fields.

--- modules/release_team/main.tf
+++ modules/release_team/main.tf
@@ -3,6 +3,10 @@ resource "github_team" "release_team" {
   description = "ROS release managers for the ${var.team_name} project"
   privacy = "closed"
   create_default_maintainer = false
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
+
 }
 
 resource "github_team_membership" "release_team" {
@@ -10,6 +14,9 @@ resource "github_team_membership" "release_team" {
   team_id = github_team.release_team.id
   username = each.value
   role = "member"
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "github_team_repository" "release_team" {
@@ -17,4 +24,7 @@ resource "github_team_repository" "release_team" {
   team_id = github_team.release_team.id
   repository = each.value
   permission = "maintain"
+  lifecycle {
+    ignore_changes = [ etag ]
+  }
 }
