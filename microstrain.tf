313324641495effbe3f7c62d71a03f968eb93818
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-01-06 13:46:28 -0800

    Add microstrain release team.

---
+++ microstrain.tf
@@ -0,0 +1,16 @@
+locals {
+  microstrain_team = [
+    "robbiefish",
+  ]
+  microstrain_repositories = [
+    "microstrain_inertial-release",
+    "ntrip_client-release",
+  ]
+}
+
+module "microstrain_team" {
+  source = "./modules/release_team"
+  team_name = "microstrain"
+  members = local.microstrain_team
+  repositories = local.microstrain_repositories
+}
