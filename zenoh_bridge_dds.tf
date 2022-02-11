67d365ea5de652d971bac07000b6149991364112
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-11 15:45:39 -0800

    Add zenoh_bridge_dds release team.
    Upstream release repository to mirror: https://github.com/atolab/zenoh_bridge_dds-release.git

---
+++ zenoh_bridge_dds.tf
@@ -0,0 +1,15 @@
+locals {
+  zenoh_bridge_dds_team = [
+    "JEnoch",
+  ]
+  zenoh_bridge_dds_repositories = [
+    "zenoh_bridge_dds-release",
+  ]
+}
+
+module "zenoh_bridge_dds_team" {
+  source = "./modules/release_team"
+  team_name = "zenoh_bridge_dds"
+  members = local.zenoh_bridge_dds_team
+  repositories = local.zenoh_bridge_dds_repositories
+}
