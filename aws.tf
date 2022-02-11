213d8bc4c43de8fe59d21af75e4bf9901edba115
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-11 13:32:28 -0800

    Add aws release team.
    Upstream release repository to mirror: https://github.com/aws-gbp/aws_robomaker_small_warehouse_world-release.git

---
+++ aws.tf
@@ -0,0 +1,15 @@
+locals {
+  aws_team = [
+    "emersonknapp",
+  ]
+  aws_repositories = [
+    "aws_robomaker_small_warehouse_world-release",
+  ]
+}
+
+module "aws_team" {
+  source = "./modules/release_team"
+  team_name = "aws"
+  members = local.aws_team
+  repositories = local.aws_repositories
+}
