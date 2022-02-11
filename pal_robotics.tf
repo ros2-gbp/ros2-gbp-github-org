802409fb5a20e8222ff100cfa75100cd6bf41efe
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-11 13:37:44 -0800

    Add pal_robotics release team.
    Upstream release repository to mirror: https://github.com/pal-gbp/backward_ros-release

---
+++ pal_robotics.tf
@@ -0,0 +1,15 @@
+locals {
+  pal_robotics_team = [
+    "v-lopez",
+  ]
+  pal_robotics_repositories = [
+    "backward_ros-release",
+  ]
+}
+
+module "pal_robotics_team" {
+  source = "./modules/release_team"
+  team_name = "pal_robotics"
+  members = local.pal_robotics_team
+  repositories = local.pal_robotics_repositories
+}
