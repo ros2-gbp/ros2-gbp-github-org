2ab1e0211b15b1003d9cf59bd780c3c2724a40da
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-11 14:21:17 -0800

    Add mavlink release team.
    Upstream release repository to mirror: https://github.com/mavlink/mavlink-gbp-release.git
    Upstream release repository to mirror: https://github.com/mavlink/mavros-release.git

---
+++ mavlink.tf
@@ -0,0 +1,16 @@
+locals {
+  mavlink_team = [
+    "vooon",
+  ]
+  mavlink_repositories = [
+    "mavlink-gbp-release",
+    "mavros-release",
+  ]
+}
+
+module "mavlink_team" {
+  source = "./modules/release_team"
+  team_name = "mavlink"
+  members = local.mavlink_team
+  repositories = local.mavlink_repositories
+}
