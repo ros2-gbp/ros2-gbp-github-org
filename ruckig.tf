da14a93d0afbc62a94526fa2745edaed62a410f7
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-11 15:28:44 -0800

    Add ruckig release team.
    Upstream release repository to mirror: https://github.com/pantor/ruckig-release.git

---
+++ ruckig.tf
@@ -0,0 +1,15 @@
+locals {
+  ruckig_team = [
+    "pantor",
+  ]
+  ruckig_repositories = [
+    "ruckig-release",
+  ]
+}
+
+module "ruckig_team" {
+  source = "./modules/release_team"
+  team_name = "ruckig"
+  members = local.ruckig_team
+  repositories = local.ruckig_repositories
+}
