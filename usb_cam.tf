eee3faed33892f1db5b3beb623dbef2fd82568c3
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-02-11 15:38:00 -0800

    Add usb_cam release team.
    Upstream release repository to mirror: https://github.com/ros-gbp/usb_cam-release.git

---
+++ usb_cam.tf
@@ -0,0 +1,15 @@
+locals {
+  usb_cam_team = [
+    "flynneva",
+  ]
+  usb_cam_repositories = [
+    "usb_cam-release",
+  ]
+}
+
+module "usb_cam_team" {
+  source = "./modules/release_team"
+  team_name = "usb_cam"
+  members = local.usb_cam_team
+  repositories = local.usb_cam_repositories
+}
