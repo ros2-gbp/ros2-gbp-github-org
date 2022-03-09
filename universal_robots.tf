ab1c2178e84d3b7a58cdb619c9a1eba2f1a7403b
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-03-09 15:54:01 -0800

    Add universal_robots release team.
    * Name of the new release team: universal_robots
    * Release repositories to add:
      * [Universal_Robots_Client_Library](https://github.com/UniversalRobots/Universal_Robots_Client_Library)
        * [x] There is an existing release repository which should be imported: https://github.com/UniversalRobots/Universal_Robots_Client_Library-release
    
    * Team members
      * [@fmauch](https://github.com/UniversalRobots/Universal_Robots_Client_Library/blob/8f20b42205f645bba406d5cf3b1163ea0a42fbc9/package.xml#L12)

---
+++ universal_robots.tf
@@ -0,0 +1,15 @@
+locals {
+  universal_robots_team = [
+    "fmauch",
+  ]
+  universal_robots_repositories = [
+    "universal_robots-release",
+  ]
+}
+
+module "universal_robots_team" {
+  source = "./modules/release_team"
+  team_name = "universal_robots"
+  members = local.universal_robots_team
+  repositories = local.universal_robots_repositories
+}
