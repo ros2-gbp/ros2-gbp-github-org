d972f83886fa46f79ed2e5edad5caae768245da4
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-03-30 11:54:39 -0700

    Add irobot_education release team.

---
+++ irobot_education.tf
@@ -0,0 +1,18 @@
+locals {
+  irobot_education_team = [
+    "alsora",
+    "justinIRBT",
+    "shamlian",
+  ]
+  irobot_education_repositories = [
+    "irobot_create_msgs-release",
+    "create3_sim-release",
+  ]
+}
+
+module "irobot_education_team" {
+  source       = "./modules/release_team"
+  team_name    = "irobot_education"
+  members      = local.irobot_education_team
+  repositories = local.irobot_education_repositories
+}
