3ed179e8a6fbe8def2341095ffd13ec5fcfb53d3
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2022-04-12 04:43:02 +0900

    Add foxglove_msgs release team.
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

---
+++ foxglove_msgs.tf
@@ -0,0 +1,16 @@
+locals {
+  foxglove_msgs_team = [
+    "jhurliman",
+    "wep21"
+  ]
+  foxglove_msgs_repositories = [
+    "ros_foxglove_msgs-release",
+  ]
+}
+
+module "foxglove_msgs_team" {
+  source       = "./modules/release_team"
+  team_name    = "foxglove_msgs"
+  members      = local.foxglove_msgs_team
+  repositories = local.foxglove_msgs_repositories
+}
