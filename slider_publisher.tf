d2523d58c64fd0052d85932b3f1b8cf16c08c666
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-05-05 18:51:32 +0200

    Add slider_publisher team (#52)
    * add slider_publisher repo
    
    * Add slider_publisher team and repositories to global lists.
    
    Co-authored-by: Steven! Ragnarök <steven@nuclearsandwich.com>

---
+++ slider_publisher.tf
@@ -0,0 +1,15 @@
+locals {
+  slider_publisher_team = [
+    "oKermorgant",
+  ]
+  slider_publisher_repositories = [
+    "slider_publisher-release",
+  ]
+}
+
+module "slider_publisher_team" {
+  source       = "./modules/release_team"
+  team_name    = "slider_publisher"
+  members      = local.slider_publisher_team
+  repositories = local.slider_publisher_repositories
+}
