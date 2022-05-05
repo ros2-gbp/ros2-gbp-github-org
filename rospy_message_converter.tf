7da5eb50b54aac285ad9a40ffb5df67ff3df2efe
Martin Günther <martin.guenther@dfki.de>
2022-05-05 18:47:38 +0200

    Add new rospy_message_converter team (#51)

---
+++ rospy_message_converter.tf
@@ -0,0 +1,15 @@
+locals {
+  rospy_message_converter_team = [
+    "mintar",
+  ]
+  rospy_message_converter_repositories = [
+    "rospy_message_converter-release",
+  ]
+}
+
+module "rospy_message_converter_team" {
+  source       = "./modules/release_team"
+  team_name    = "rospy_message_converter"
+  members      = local.rospy_message_converter_team
+  repositories = local.rospy_message_converter_repositories
+}
