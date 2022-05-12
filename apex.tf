3d95e0ff01a5d3a81c839e54f6babb4984321f36
Evan Flynn <6157095+flynneva@users.noreply.github.com>
2022-05-12 14:35:05 -0700

    Update Apex.AI team with new members
    - create performance_test-release repo

--- apex.tf
+++ apex.tf
@@ -1,11 +1,15 @@
 locals {
   apex_team = [
     "carlossvg",
+    "flynneva",
+    "erikatapex",
+    "revati-naik",
   ]
   apex_repositories = [
     "apex_containers-release",
     "apex_test_tools-release",
     "point_cloud_msg_wrapper-release",
+    "performance_test-release",
   ]
 }
 
