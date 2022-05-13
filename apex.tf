90252666513699748353f3a812040470cafbe793
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-05-13 10:47:11 -0700

    Fix lexical sort order.

--- apex.tf
+++ apex.tf
@@ -1,15 +1,15 @@
 locals {
   apex_team = [
     "carlossvg",
-    "flynneva",
     "erikatapex",
+    "flynneva",
     "revati-naik",
   ]
   apex_repositories = [
     "apex_containers-release",
     "apex_test_tools-release",
-    "point_cloud_msg_wrapper-release",
     "performance_test-release",
+    "point_cloud_msg_wrapper-release",
   ]
 }
 
