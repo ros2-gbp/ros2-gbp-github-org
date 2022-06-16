19c31b6212247f8f13c313f81fccd2c944f48734
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2022-06-16 11:16:23 -0700

    Add robot_calibration to the aggregate repositories and members. (#72)

--- 00-members.tf
+++ 00-members.tf
@@ -69,6 +69,7 @@ locals {
       local.realsense_team,
       local.rmf_team,
       local.roboception_team,
+      local.robot_calibration_team,
       local.robot_localization_team,
       local.robotis_team,
       local.robotwebtools_team,
