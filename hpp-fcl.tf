b285e6feeae339395e19a94af8379321565aad4f
Wolfgang Merkt <w.merkt@gmail.com>
2022-06-29 16:37:19 +0100

    Add hpp-fcl.tf

---
+++ hpp-fcl.tf
@@ -0,0 +1,19 @@
+locals {
+  hpp_fcl_team = [
+    "jcarpent",
+    "jmirabel",
+    "wxmerkt",
+    "nim65s",
+  ]
+  hpp_fcl_repositories = [
+    "hpp-fcl-release",
+  ]
+}
+  
+module "hpp_fcl_team" {
+  source       = "./modules/release_team"
+  team_name    = "hpp-fcl"
+  members      = local.hpp_fcl_team
+  repositories = local.hpp_fcl_repositories
+  depends_on   = [github_membership.members, github_repository.repositories]
+}
