62f394cac8cf860026b3d4acffccafee4d526e11
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-06-29 15:56:24 -0700

    Use underscore in hpp_fcl team and release repo name.
    For consistency with the ROS package naming conventions let's use the
    underscore as the separator in names.

--- hpp_fcl.tf
+++ hpp_fcl.tf
@@ -6,13 +6,13 @@ locals {
     "nim65s",
   ]
   hpp_fcl_repositories = [
-    "hpp-fcl-release",
+    "hpp_fcl-release",
   ]
 }
-  
+
 module "hpp_fcl_team" {
   source       = "./modules/release_team"
-  team_name    = "hpp-fcl"
+  team_name    = "hpp_fcl"
   members      = local.hpp_fcl_team
   repositories = local.hpp_fcl_repositories
   depends_on   = [github_membership.members, github_repository.repositories]
