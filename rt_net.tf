7f6b8db7ba1eb44b25ecb540611a5dc331760c4e
Scott K Logan <logans@cottsay.net>
2022-08-10 11:23:47 -0700

    Migrate 5 rt-net-gbp repositories to ros2-gbp

--- rt_net.tf
+++ rt_net.tf
@@ -7,6 +7,11 @@ locals {
   rt_net_repositories = [
     "raspimouse2-release",
     "raspimouse_description-release",
+    "raspimouse_ros2_examples-release",
+    "raspimouse_slam_navigation_ros2-release",
+    "rt_manipulators_cpp-release",
+    "rt_usb_9axisimu_driver-release",
+    "crane_plus-release",
   ]
 }
 
