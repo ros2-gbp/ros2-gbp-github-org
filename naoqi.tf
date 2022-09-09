30803f16c7aa3499d4120a43218416aa43e45ede
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-09-09 12:16:57 -0700

    Add naoqi team.

---
+++ naoqi.tf
@@ -0,0 +1,26 @@
+locals {
+  naoqi_team = [
+    "Karsten1987",
+    "mbusy",
+    "mikaelarguedas",
+    "nlyubova",
+    "suryaambrose",
+    "zygopter",
+  ]
+  naoqi_repositories = [
+    "libqi-release",
+    "libqicore-release",
+    "nao_meshes-release",
+    "naoqi_bridge_msgs-release",
+    "naoqi_driver-release",
+    "pepper_meshes-release",
+  ]
+}
+
+module "naoqi_team" {
+  source       = "./modules/release_team"
+  team_name    = "naoqi"
+  members      = local.naoqi_team
+  repositories = local.naoqi_repositories
+  depends_on   = [github_membership.members, github_repository.repositories]
+}
