6fa9ad2c4e4e80e84150877c8284a4f6810004d9
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2023-01-13 16:26:21 -0800

    Add schunk_svh releaes team. (#175)

--- 00-repositories.tf
+++ 00-repositories.tf
@@ -96,6 +96,7 @@ locals {
     local.rt_net_repositories,
     local.rtabmap_repositories,
     local.ruckig_repositories,
+    local.schunk_svh_repositories,
     local.security_wg_repositories,
     local.simple_launch_repositories,
     local.skylove_repositories,
