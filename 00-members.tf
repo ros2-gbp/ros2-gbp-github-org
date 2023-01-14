1b2e68c4f3332823b09374012e3ecb108d47a962
Steven! Ragnarök <steven@nuclearsandwich.com>
2023-01-13 16:25:31 -0800

    Add schunk_svh releaes team.

--- 00-members.tf
+++ 00-members.tf
@@ -97,6 +97,7 @@ locals {
       local.rt_net_team,
       local.rtabmap_team,
       local.ruckig_team,
+      local.schunk_svh_team,
       local.security_wg_team,
       local.simple_launch_team,
       local.skylove_team,
---
+++ schunk_svh.tf
@@ -0,0 +1,17 @@
+locals {
+  schunk_svh_team = [
+    "stefanscherzinger",
+  ]
+  schunk_svh_repositories = [
+    "schunk_svh_library-release",
+    "schunk_svh_ros_driver-release",
+  ]
+}
+
+module "schunk_svh_team" {
+  source       = "./modules/release_team"
+  team_name    = "schunk_svh"
+  members      = local.schunk_svh_team
+  repositories = local.schunk_svh_repositories
+  depends_on   = [github_membership.members, github_repository.repositories]
+}
