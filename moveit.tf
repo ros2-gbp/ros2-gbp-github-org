1d53bdbb8442bc21cf91f1e1c7c7d951cd0ea629
Robert Haschke <rhaschke@users.noreply.github.com>
2023-02-11 02:01:05 +0100

    Add MTC to moveit_repositories (#191)
    * Add MTC to moveit_repositories
    
    * Fix lexicographical sort order.
    
    ---------
    
    Co-authored-by: Steven! Ragnarök <steven@nuclearsandwich.com>

--- moveit.tf
+++ moveit.tf
@@ -14,6 +14,7 @@ locals {
     "moveit2-release",
     "moveit_msgs-release",
     "moveit_resources-release",
+    "moveit_task_constructor-release",
     "moveit_visual_tools-release",
     "pick_ik-release",
     "random_numbers-release",
