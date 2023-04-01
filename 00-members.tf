72fe9a1f56d7f96e16aad5b09153e2782306bc49
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2023-03-31 19:02:43 -0700

    Add introlab release team and repositories. (#224)

--- 00-members.tf
+++ 00-members.tf
@@ -47,6 +47,7 @@ locals {
       local.ijnek_team,
       local.image_transport_plugins_team,
       local.imu_tools_team,
+      local.introlab_team,
       local.irobot_education_team,
       local.josh_newans_team,
       local.kobuki_base_team,
--- 00-repositories.tf
+++ 00-repositories.tf
@@ -46,6 +46,7 @@ locals {
     local.ijnek_repositories,
     local.image_transport_plugins_repositories,
     local.imu_tools_repositories,
+    local.introlab_repositories,
     local.irobot_education_repositories,
     local.josh_newans_repositories,
     local.kobuki_base_repositories,
