ad984737d5b0905e37ded032e9f9f2a25ac5768f
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2023-07-28 17:39:14 -0700

    Add release team for ros_event_camera. (#307)

---
+++ ros_event_camera.tf
@@ -0,0 +1,18 @@
+locals {
+  ros_event_camera_team = [
+    "berndpfrommer",
+    "k-chaney",
+  ]
+  ros_event_camera_repositories = [
+    "event_camera_msgs-release",
+    "metavision_driver-release",
+  ]
+}
+
+module "ros_event_camera_team" {
+  source       = "./modules/release_team"
+  team_name    = "ros_event_camera"
+  members      = local.ros_event_camera_team
+  repositories = local.ros_event_camera_repositories
+  depends_on   = [github_membership.members, github_repository.repositories]
+}
