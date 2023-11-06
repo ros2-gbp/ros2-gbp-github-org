77866d75637571eda17b9885d10d9e9611b3e243
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2023-11-06 16:28:53 -0500

    Update 00-repositories.tf

--- 00-repositories.tf
+++ 00-repositories.tf
@@ -75,6 +75,7 @@ locals {
     local.mrpt2_repositories,
     local.mrt_cmake_modules_repositories,
     local.mobile_catkin_modules_build_development_tools_repositories,
+    local.mobile_message_service_generator_repositories,
     local.naoqi_repositories,
     local.navigation_repositories,
     local.neobotix_repositories,
