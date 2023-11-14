01573eddb6abdce4c47c361774dfe30d6ac7e660
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2023-11-13 20:41:49 -0500

    Update humanoid_robot_module.tf

--- humanoid_robot_module.tf
+++ humanoid_robot_module.tf
@@ -1,16 +1,16 @@
 locals {
-  humanoid_robot_module_team = [
+  humanoid_robot_intelligence_control_system_module_team = [
     "RonaldsonBellande",
   ]
-  humanoid_robot_module_repositories = [
-    "humanoid_robot_module-release",
+  humanoid_robot_intelligence_control_system_module_repositories = [
+    "humanoid_robot_intelligence_control_system_module-release",
   ]
 }
 
-module "humanoid_robot_module_team" {
+module "humanoid_robot_intelligence_control_system_module_team" {
   source       = "./modules/release_team"
-  team_name    = "humanoid_robot_module"
-  members      = local.humanoid_robot_module_team
-  repositories = local.humanoid_robot_module_repositories
+  team_name    = "humanoid_robot_intelligence_control_system_module"
+  members      = local.humanoid_robot_intelligence_control_system_module_team
+  repositories = local.humanoid_robot_intelligence_control_system_module_repositories
   depends_on   = [github_membership.members, github_repository.repositories]
 }
