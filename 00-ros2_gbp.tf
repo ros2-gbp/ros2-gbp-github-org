7cd11af0836d457144c0aa089fdbc52855603897
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2023-11-20 11:07:00 -0800

    Add a .github repository for organization-level documentation. (#380)
    * Add a .github repository for organization-level documentation.
    
    terraform fmt.
    
    * Fix ros2_gbp docs team name.
    
    * Use property instead of string.

---
+++ 00-ros2_gbp.tf
@@ -0,0 +1,46 @@
+locals {
+  ros2_gbp_docs_team = [
+    "kscottz",
+  ]
+}
+
+resource "github_repository" "dotgithub" {
+  name       = ".github"
+  visibility = "public"
+  lifecycle {
+    # Plans that destroy repository releases will delete the repository on
+    # GitHub and that shouldn't be done in the normal course of operation.
+    prevent_destroy = true
+    # Ignore fields that are not set or managed by this terraform project
+    # to prevent unecessary drift.
+    ignore_changes = [
+      allow_merge_commit,
+      description,
+      has_downloads,
+      has_projects,
+      has_issues,
+      has_wiki,
+      vulnerability_alerts
+    ]
+  }
+}
+
+resource "github_team" "docs_team" {
+  name                      = "ros2-gbp-docs"
+  description               = "ros2-gbp documentation contributors"
+  privacy                   = "closed"
+  create_default_maintainer = false
+}
+
+resource "github_team_membership" "docs_team" {
+  for_each = toset(local.ros2_gbp_docs_team)
+  team_id  = github_team.docs_team.id
+  username = each.value
+  role     = "member"
+}
+
+resource "github_team_repository" "docs_team" {
+  team_id    = github_team.docs_team.id
+  repository = github_repository.dotgithub.name
+  permission = "maintain"
+}
