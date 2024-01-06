90f329af0425bcc55e95c3fec0e84732464d07f7
Scott K Logan <logans@cottsay.net>
2024-01-05 19:13:51 -0600

    Add orco team (#416)

--- 00-members.tf
+++ 00-members.tf
@@ -96,6 +96,7 @@ locals {
     local.odri_team,
     local.ompl_team,
     local.openni2_camera_team,
+    local.orco_team,
     local.ouster_drivers_team,
     local.ouster_team,
     local.ouxt_team,
