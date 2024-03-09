24a27f688341593474988150b16920ea1a2f1480
Steven! Ragnarök <steven@nuclearsandwich.com>
2024-03-08 16:44:28 -0800

    Update ika_rwth_aachen release team and repositories.

--- ika_rwth_aachen.tf
+++ ika_rwth_aachen.tf
@@ -1,8 +1,11 @@
 locals {
   ika_rwth_aachen_team = [
+    "gkueppers",
+    "jpbusch",
     "lreiher",
   ]
   ika_rwth_aachen_repositories = [
+    "etsi_its_messages-release",
     "message_tf_frame_transformer-release",
     "mqtt_client-release",
   ]
