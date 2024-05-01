a6811307555e2c2343e2f406471849da4d370c20
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-05-01 13:32:40 +0900

    update autoware
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- autoware.tf
+++ autoware.tf
@@ -1,11 +1,18 @@
 locals {
   autoware_team = [
+    "esteve",
     "mitsudome-r",
+    "youtalk",
   ]
 
   autoware_repositories = [
     "acado_vendor-release",
+    "autoware_adapi_msgs-release",
     "autoware_auto_msgs-release",
+    "autoware_cmake-release",
+    "autoware_lanelet2_extension-release",
+    "autoware_msgs-release",
+    "autoware_utils-release",
     "qpoases_vendor-release",
     "ros2_socketcan-release",
     "tvm_vendor-release",
