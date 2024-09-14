824bec6defc6d692c91c65553409d846335d5b03
Steven! Ragnarök <steven@nuclearsandwich.com>
2024-09-13 21:58:07 -0700

    Add foxglove_compressed_video_transport release repository.

--- misc_utilities.tf
+++ misc_utilities.tf
@@ -9,6 +9,7 @@ locals {
     "ffmpeg_image_transport_msgs-release",
     "ffmpeg_image_transport_tools-release",
     "flex_sync-release",
+    "foxglove_compressed_video_transport-release",
   ]
 }
 
