fa82423a8d0ceb11691c22412fea3fa3205064c5
Steven! Ragnarök <steven@nuclearsandwich.com>
2024-12-14 07:47:32 -0800

    I missed two.

--- 4am_robotics.tf
+++ 4am_robotics.tf
@@ -11,7 +11,7 @@ locals {
 module "_4am_robotics_team" {
   source       = "./modules/release_team"
   team_name    = "4am-robotics"
-  members      = local.4am_robotics_team
-  repositories = local.4am_robotics_repositories
+  members      = local._4am_robotics_team
+  repositories = local._4am_robotics_repositories
   depends_on   = [github_membership.members, github_repository.repositories]
 }
