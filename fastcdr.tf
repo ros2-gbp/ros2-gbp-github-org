62e325a51bac32aeb0c8fcbb79af0256bbb43331
Miguel Company <miguelcompany@eprosima.com>
2025-03-06 09:40:02 +0100

    Add `rosidl_dynamic_typesupport_fastrtps` to `fastcdr.tf`
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- fastcdr.tf
+++ fastcdr.tf
@@ -9,6 +9,7 @@ locals {
     "foonathan_memory_vendor-release",
     "rmw_fastrtps-release",
     "rosidl_typesupport_fastrtps-release",
+    "rosidl_dynamic_typesupport_fastrtps-release",
   ]
 }
 
