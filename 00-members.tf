d896e73b4206d73a32f512a62f233e3bc1173e22
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-04-12 12:49:32 -0700

    Add wasp_research_group release team and repositories.

--- 00-members.tf
+++ 00-members.tf
@@ -231,6 +231,7 @@ locals {
     local.visp_team,
     local.vrpn_team,
     local.wasp_research_group_team,
+    local.wasp_research_group_team,
     local.wep21_team,
     local.xacro_team,
     local.yukkysaito_team,
