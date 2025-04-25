ec336f654a612afa756b4e0f2097bfa522e00446
Samer Khshiboun <99127997+SamerKhshiboun@users.noreply.github.com>
2025-04-25 21:19:18 +0300

    Update Intel RealSense team and repositories (#297)
    * Update Intel RealSense team and repositories
    
    * Archive librealsense-release repository.
    
    * Update membership based on #725.
    
    * Also archive ros2_intel_realsense release repository.
    
    ---------
    
    Co-authored-by: Steven! Ragnarök <steven@nuclearsandwich.com>

--- realsense.tf
+++ realsense.tf
@@ -1,10 +1,12 @@
 locals {
   realsense_team = [
-    "doronhi",
+    "IntelRealSense",
+    "Nir-Az",
+    "ashrafk93",
   ]
   realsense_repositories = [
-    "librealsense-release",
-    "ros2_intel_realsense-release",
+    "librealsense2-release",
+    "realsense-ros-release",
   ]
 }
 
