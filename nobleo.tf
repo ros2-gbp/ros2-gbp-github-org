e2466f71911d6af615f6347b0adfa2f71ed846fc
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2025-10-10 20:23:30 -0700

    Add mp_units_vendor release repository. (#889)

--- nobleo.tf
+++ nobleo.tf
@@ -9,6 +9,7 @@ locals {
     "libnabo-release",
     "libpointmatcher-release",
     "magic_enum-release",
+    "mp_units_vendor-release",
     "nobleo_socketcan_bridge-release",
     "ros2_fmt_logger-release",
     "rslidar_msg-release",
