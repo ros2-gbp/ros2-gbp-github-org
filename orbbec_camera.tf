5f74ad3c4fb1b9aa9907ecda096ded2b31144746
ob-yalian <luotao2025@163.com>
2025-10-30 16:26:49 +0800

    Add orbbec_camera team and repositories to membership configuration

---
+++ orbbec_camera.tf
@@ -0,0 +1,17 @@
+locals {
+  orbbec_camera_team = [
+    "ob-yalian",
+  ]
+
+  orbbec_camera_repositories = [
+    "orbbec_camera_v2-release",
+  ]
+}
+
+module "orbbec_camera" {
+  source       = "./modules/release_team"
+  team_name    = "orbbec_camera"
+  members      = local.orbbec_camera_team
+  repositories = local.orbbec_camera_repositories
+  depends_on   = [github_membership.members, github_repository.repositories]
+}
