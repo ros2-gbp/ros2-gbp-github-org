548be329f56a20a99448b915282c47b89ed1a3df
Scott K Logan <logans@cottsay.net>
2025-12-05 13:50:25 -0600

    Add 'persist_parameter_server-release' to 'sony_rdc' team (#926)

--- sony_rdc.tf
+++ sony_rdc.tf
@@ -4,6 +4,7 @@ locals {
     "fujitatomoya",
   ]
   sony_rdc_repositories = [
+    "persist_parameter_server-release",
     "rcl_logging_syslog-release",
     "service_load_balancing-release",
   ]
