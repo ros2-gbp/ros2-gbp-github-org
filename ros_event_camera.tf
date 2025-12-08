b4f6b7626266b0e625bc221e7504f22606aeacf9
Scott K Logan <logans@cottsay.net>
2025-12-08 09:42:11 -0600

    Add 'frequency_cam-release' repo to 'ros_event_camera' team

--- ros_event_camera.tf
+++ ros_event_camera.tf
@@ -10,6 +10,7 @@ locals {
     "event_camera_renderer-release",
     "event_image_reconstruction_fibar-release",
     "fibar_lib-release",
+    "frequency_cam-release",
     "libcaer_driver-release",
     "libcaer_vendor-release",
     "metavision_driver-release",
