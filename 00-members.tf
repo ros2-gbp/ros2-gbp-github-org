bda3e07da94b10168a1d6343feda51ea9a2e5de8
Scott K Logan <logans@cottsay.net>
2026-02-19 13:29:08 -0600

    Add 'ros2_snapshot' team (#991)
    Initial members:
    - dcconner
    
    Initial repositories:
    - ros2_snapshot-release

--- 00-members.tf
+++ 00-members.tf
@@ -193,6 +193,7 @@ locals {
     local.robotwebtools_team,
     local.ros2_gbp_docs_team,
     local.ros2_rust_team,
+    local.ros2_snapshot_team,
     local.ros2tree_team,
     local.ros4hri_team,
     local.ros_admins,
