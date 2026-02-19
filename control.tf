8242965ad308dc359c4d3e57d3c6d1d01c1f0a0b
Scott K Logan <logans@cottsay.net>
2026-02-19 10:40:33 -0600

    Add 'mujoco_ros2_control-release' to 'control' team

--- control.tf
+++ control.tf
@@ -12,6 +12,7 @@ locals {
     "gazebo_ros2_control-release",
     "ign_ros2_control-release",
     "kinematics_interface-release",
+    "mujoco_ros2_control-release",
     "realtime_tools-release",
     "ros2_control-release",
     "ros2_control_cmake-release",
