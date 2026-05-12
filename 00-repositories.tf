a86d435945d64a77cfd5a2e5ff24bbd2c8a74c9a
Scott K Logan <logans@cottsay.net>
2026-05-11 20:01:08 -0400

    Add 'accelerated_memory_transports' team (#1035)
    Initial members:
    - karsten-nvidia
    - nvcyc
    - yuanknv
    
    Initial repositories:
    - accelerated_memory_transports_repositories
    - rosidl_buffer_backends-release
    - rosidl_buffer_backends_tutorials-release

--- 00-repositories.tf
+++ 00-repositories.tf
@@ -4,6 +4,7 @@ locals {
     local._42dot_repositories,
     local._4am_robotics_repositories,
     local._archived_repositories,
+    local.accelerated_memory_transports_repositories,
     local.acceleration_wg_repositories,
     local.ackermann_msgs_repositories,
     local.aerostack2_repositories,
