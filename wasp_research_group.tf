496cd75d1b40b021701bb5a818e7fccbf1933a6f
Scott K Logan <logans@cottsay.net>
2026-08-24 17:11:51 -0500

    Add 'rqt_lifecycle_manager-release' to 'wasp_research_group' team

--- wasp_research_group.tf
+++ wasp_research_group.tf
@@ -5,6 +5,7 @@ locals {
   ]
   wasp_research_group_repositories = [
     "laser_segmentation-release",
+    "rqt_lifecycle_manager-release",
     "sicks300_ros2-release",
     "slg_msgs-release",
   ]
