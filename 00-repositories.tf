c13f2a6ddf694327ee1de66c37db37e5e4e10290
Scott K Logan <logans@cottsay.net>
2026-08-24 19:00:57 -0500

    Add 'robstride_ros2' team
    Initial members:
    - s2015-turtle
    
    Initial repositories:
    - robstride_ros2-release

--- 00-repositories.tf
+++ 00-repositories.tf
@@ -205,6 +205,7 @@ locals {
     local.robotont_repositories,
     local.robotraconteur_repositories,
     local.robotwebtools_repositories,
+    local.robstride_ros2_repositories,
     local.ros2_medkit_repositories,
     local.ros2_rust_repositories,
     local.ros2_snapshot_repositories,
